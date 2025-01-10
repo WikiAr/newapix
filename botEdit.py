e817756fd52a91905ca705cd1f4c0629a0cad2fb
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2025-01-10 05:29:15 +0300

    1 (#83)

--- botEdit.py
+++ botEdit.py
@@ -8,29 +8,30 @@ from newapi import printe
 from newapi import txtlib
 
 # ---
-edit_username = {
-    1: "Mr.Ibrahembot"
-}
+edit_username = {1: "Mr.Ibrahembot"}
 # ---
 Bot_Cash = {}
 # ---
 stop_edit_temps = {
-    'all': ['تحرر', 'قيد التطوير', 'يحرر'],
-    'fixref': ["لا لصيانة المراجع"],
-    'cat': ["لا للتصنيف المعادل"],
-    'stub': ["لا لتخصيص البذرة"],
-    'tempcat': ["لا لإضافة صناديق تصفح معادلة"],
-    'portal': ["لا لربط البوابات المعادل", 'لا لصيانة البوابات']
+    "all": ["تحرر", "قيد التطوير", "يحرر"],
+    "تعريب": ["لا للتعريب"],
+    "تقييم آلي": ["لا للتقييم الآلي"],
+    "reftitle": ["لا لعنونة مرجع غير معنون"],
+    "fixref": ["لا لصيانة المراجع"],
+    "cat": ["لا للتصنيف المعادل"],
+    "stub": ["لا لتخصيص البذرة"],
+    "tempcat": ["لا لإضافة صناديق تصفح معادلة"],
+    "portal": ["لا لربط البوابات المعادل", "لا لصيانة البوابات"],
 }
 
 
-def bot_May_Edit(text='', title_page='', botjob='all'):
+def bot_May_Edit(text="", title_page="", botjob="all"):
     # ---
-    if ('botedit' in sys.argv or 'editbot' in sys.argv) or 'workibrahem' in sys.argv:
+    if ("botedit" in sys.argv or "editbot" in sys.argv) or "workibrahem" in sys.argv:
         return True
     # ---
-    if botjob in ['', 'fixref|cat|stub|tempcat|portal']:
-        botjob = 'all'
+    if botjob in ["", "fixref|cat|stub|tempcat|portal"]:
+        botjob = "all"
     # ---
     if botjob not in Bot_Cash:
         Bot_Cash[botjob] = {}
@@ -40,10 +41,10 @@ def bot_May_Edit(text='', title_page='', botjob='all'):
     # ---
     templates = txtlib.extract_templates_and_params(text)
     # ---
-    all_stop = stop_edit_temps['all']
+    all_stop = stop_edit_temps["all"]
     # ---
     for temp in templates:
-        _name, namestrip, params, _template = temp['name'], temp['namestrip'], temp['params'], temp['item']
+        _name, namestrip, params, _template = temp["name"], temp["namestrip"], temp["params"], temp["item"]
         title = namestrip
         # ---
         # printe.output( '<<lightred>>botEdit.py title:(%s), params:(%s).' % ( title,str(params) ) )
@@ -51,29 +52,29 @@ def bot_May_Edit(text='', title_page='', botjob='all'):
         restrictions = stop_edit_temps.get(botjob, [])
         # ---
         if title in restrictions or title in all_stop:
-            printe.output(f'<<lightred>> botEdit.py: the page has temp:({title}), botjob:{botjob} skipp.')
+            printe.output(f"<<lightred>> botEdit.py: the page has temp:({title}), botjob:{botjob} skipp.")
             Bot_Cash[botjob][title_page] = False
             return False
         # ---
         # if title == 'Nobots' or title == 'nobots':
-        if title.lower() in ['bots', 'nobots']:
+        if title.lower() in ["bots", "nobots"]:
             # ---
             # printe.output( '<<lightred>>botEdit.py title:(%s), params:(%s).' % ( title,str(params) ) )
             # ---
-            if title.lower() == 'nobots':
+            if title.lower() == "nobots":
                 # ---
                 # {{nobots}}                منع جميع البوتات
                 # منع جميع البوتات
                 if not params:
-                    printe.output(f'<<lightred>> botEdit.py: the page has temp:({_template}), botjob:{botjob} skipp.')
+                    printe.output(f"<<lightred>> botEdit.py: the page has temp:({_template}), botjob:{botjob} skipp.")
                     # pywikibot.output( 'return False 2 ' )
                     Bot_Cash[botjob][title_page] = False
                     return False
-                elif params.get('1'):
-                    List = [x.strip() for x in params.get('1', '').split(',')]
+                elif params.get("1"):
+                    List = [x.strip() for x in params.get("1", "").split(",")]
                     # if 'all' in List or pywikibot.calledModuleName() in List or edit_username[1] in List:
-                    if 'all' in List or edit_username[1] in List:
-                        printe.output(f'<<lightred>> botEdit.py: the page has temp:({_template}), botjob:{botjob} skipp.')
+                    if "all" in List or edit_username[1] in List:
+                        printe.output(f"<<lightred>> botEdit.py: the page has temp:({_template}), botjob:{botjob} skipp.")
                         # pywikibot.output( 'return False 3 ' )
                         Bot_Cash[title_page] = False
                         return False
@@ -81,26 +82,26 @@ def bot_May_Edit(text='', title_page='', botjob='all'):
             # {{bots|allow=<botlist>}}  منع جميع البوتات غير الموجودة في القائمة
             # {{bots|deny=<botlist>}}   منع جميع البوتات الموجودة في القائمة
             # ---
-            elif title.lower() == 'bots':
+            elif title.lower() == "bots":
                 # printe.output( 'title == (%s) ' % title )
                 # {{bots}}                  السماح لجميع البوتات
                 if not params:
                     Bot_Cash[botjob][title_page] = False
                     return False
                 else:
-                    printe.output(f'botEdit.py title:({title}), params:({str(params)}).')
+                    printe.output(f"botEdit.py title:({title}), params:({str(params)}).")
                     # for param in params:
                     # value = params[param]
                     # value = [ x.strip() for x in value.split(',') ]
                     # ---
                     # {{bots|allow=all}}      السماح لجميع البوتات
                     # {{bots|allow=none}}     منع جميع البوتات
-                    allow = params.get('allow')
+                    allow = params.get("allow")
                     if allow:
-                        value = [x.strip() for x in allow.split(',')]
+                        value = [x.strip() for x in allow.split(",")]
                         # if param == 'allow':
                         # 'all' in value or edit_username[1] in value is True
-                        sd = 'all' in value or edit_username[1] in value
+                        sd = "all" in value or edit_username[1] in value
                         if not sd:
                             printe.output(f"<<lightred>>botEdit.py Template:({title}) has |allow={','.join(value)}.")
                         else:
@@ -110,12 +111,12 @@ def bot_May_Edit(text='', title_page='', botjob='all'):
                         # ---
                     # ---
                     # {{bots|deny=all}}      منع جميع البوتات
-                    deny = params.get('deny')
+                    deny = params.get("deny")
                     if deny:
-                        value = [x.strip() for x in deny.split(',')]
+                        value = [x.strip() for x in deny.split(",")]
                         # {{bots|deny=all}}
                         # if param == 'deny':
-                        sd = 'all' not in value and edit_username[1] not in value
+                        sd = "all" not in value and edit_username[1] not in value
                         if not sd:
                             printe.output(f"<<lightred>>botEdit.py Template:({title}) has |deny={','.join(value)}.")
                         Bot_Cash[botjob][title_page] = sd
@@ -137,13 +138,13 @@ def bot_May_Edit(text='', title_page='', botjob='all'):
 # ---
 # python3 core8/pwb.py API/botEdit
 # ---
-if __name__ == '__main__':
-    texts = '''
+if __name__ == "__main__":
+    texts = """
 {{Bots|deny=all}}
 {{يتيمة|تاريخ=مايو 2020}}
 {{صندوق معلومات شخص
 | الصورة = Correggio, Alexandru Bogdan-Piteşti.jpg
-}}'''
+}}"""
     fg = bot_May_Edit(text=texts)
     print(fg)
 # ---
