7e13dd3ace41334427603abd9adf6af06ddcebbf
ibrahem <ibrahem.al-radaei@outlook.com>
2025-05-03 03:31:33 +0300

    Update pformat.py

--- pformat.py
+++ pformat.py
@@ -6,12 +6,6 @@ import wikitextparser as wtp
 from pathlib import Path
 
 
-
-# ---
-sys.argv.append('workibrahem')
-sys.argv.append('ask')
-# ---
-# ---
 # python3 core8/pwb.py newapi/pformat -title:قالب:Cycling_race/stageclassification3
 # python3 core8/pwb.py newapi/pformat -title:قالب:معلومات_خاصية_ويكي_بيانات/تتبع
 # python3 core8/pwb.py newapi/pformat -title:
@@ -35,33 +29,37 @@ def make_new_text(text):
     return new_text
 
 
-# ---
-Dir = Path(__file__).parent
-# ---
-title = ''
-text = open(f"{Dir}/pformat.txt", "r", encoding="utf-8").read()
-for arg in sys.argv:
-    arg, _, value = arg.partition(':')
-    if arg == '-title' or arg == '-page':
-        title = value
-# ---
-if title:
+if __name__ == "__main__":
     # ---
-    from newapi.page import MainPage
+    sys.argv.append('workibrahem')
+    sys.argv.append('ask')
+    # ---
+    Dir = Path(__file__).parent
+    # ---
+    title = ''
+    text = open(f"{Dir}/pformat.txt", "r", encoding="utf-8").read()
+    for arg in sys.argv:
+        arg, _, value = arg.partition(':')
+        if arg == '-title' or arg == '-page':
+            title = value
+    # ---
+    if title:
+        # ---
+        from newapi.page import MainPage
 
-    page = MainPage(title, 'ar', family='wikipedia')
-    text = page.get_text()
-    newtext = make_new_text(text)
-    if 'save' in sys.argv:
-        save_page = page.save(newtext=newtext, summary='', nocreate=1, minor='')
-else:
-    prased = wtp.parse(text)
-    newtext = prased.pformat()
-# ---
-print(newtext)
-# ---
-with open(f"{Dir}/pformat.txt", "w", encoding="utf-8") as logfile:
-    logfile.write(newtext)
-# ---
-print('add "save" to sys.argv to save.')
-# ---
+        page = MainPage(title, 'ar', family='wikipedia')
+        text = page.get_text()
+        newtext = make_new_text(text)
+        if 'save' in sys.argv:
+            save_page = page.save(newtext=newtext, summary='', nocreate=1, minor='')
+    else:
+        prased = wtp.parse(text)
+        newtext = prased.pformat()
+    # ---
+    print(newtext)
+    # ---
+    with open(f"{Dir}/pformat.txt", "w", encoding="utf-8") as logfile:
+        logfile.write(newtext)
+    # ---
+    print('add "save" to sys.argv to save.')
+    # ---
